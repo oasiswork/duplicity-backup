0c8813057a6c575f8729e52e2a1c80b47db1367c
Nicolas Brisac <nbrisac@oasiswork.fr>
2015-08-23 22:10:47 +0200

    Fixed command line syntax with multiple keys

--- duplicity-backup.sh
+++ duplicity-backup.sh
@@ -217,12 +217,12 @@ if [ "$ENCRYPTION" = "yes" ]; then
   if [ ! -z "$GPG_ENC_KEY" ] && [ ! -z "$GPG_SIGN_KEY" ]; then
     GPG_ENC_KEYS_ARRAY=( $( gpg_split_keyset ${GPG_ENC_KEY} ) )
     if [ "$HIDE_KEY_ID" = "yes" ]; then
-      ENCRYPT="--hidden-encrypt-key=${GPG_ENC_KEYS_ARRAY[@]}"
+      ENCRYPT="--hidden-encrypt-key=\"${GPG_ENC_KEYS_ARRAY[@]}\""
       if [ "$COMMAND" != "restore" -a "$COMMAND" != "restore-file" -a "$COMMAND" != "restore-dir" ]; then
         ENCRYPT="$ENCRYPT --sign-key=${GPG_SIGN_KEY}"
       fi
     else
-      ENCRYPT="--encrypt-key=${GPG_ENC_KEYS_ARRAY[@]} --sign-key=${GPG_SIGN_KEY}"
+      ENCRYPT="--encrypt-key=\"${GPG_ENC_KEYS_ARRAY[@]}\" --sign-key=${GPG_SIGN_KEY}"
     fi
     if [ ! -z "$SECRET_KEYRING" ]; then
       KEYRING="--secret-keyring ${SECRET_KEYRING}"
